e53ef3908f999813b805f31f47a9088a66c6d85a
PJ <67512968+pjdidonato@users.noreply.github.com>
2020-09-08 23:11:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -82,6 +82,23 @@ height: 200px; /* only if you want fixed height */
     break;
     case "register":
       $heading = "Register";
+      $body ='<form class="form-signin" >
+
+      <h1 class="h3 mb-3 font-weight-normal"> Please Sign In</hi1>
+      <label for="inputEmail" class="sr-only"> Email Address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3"></div>
+      <label>
+          <input type="checkbox" value="remember-me">
+          "Remember me "
+      </label>
+      </div> 
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted"> 2017-2018</p>
+      
+      </form>"';
     break;
     case "now":
       $heading = "Now";
@@ -126,13 +143,7 @@ height: 200px; /* only if you want fixed height */
     <button type="button" class="btn btn-primary" style="background-color: black; width: 100px"> link</button></div>
 <div class="float-right"> link</div>
 <div class="card"> <div class="card-body"> 
-<?php
-        
-        if ($_GET['n'] == 'register') {
-            include("register.php");
-        } 
-       
-?>
+
 
 
 
