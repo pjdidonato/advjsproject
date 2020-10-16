9825b45e4e320d5a5f8080582158abf98c5f30bd
pdidonato <didona59@students.rowan.edu>
2020-10-16 02:01:50 -0400

    moved website template into template folder

---
+++ template/example4.php
@@ -0,0 +1,30 @@
+<table class="table table-light">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Example</th>
+      <th scope="col">Example</th>
+      <th scope="col">Example</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Example</td>
+      <td>Example</td>
+      <td>Example</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Example</td>
+      <td>Example</td>
+      <td>Example</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Example</td>
+      <td>Example</td>
+      <td>Example</td>
+    </tr>
+  </tbody>
+</table>
